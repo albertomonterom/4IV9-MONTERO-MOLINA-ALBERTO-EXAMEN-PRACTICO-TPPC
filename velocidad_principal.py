c70ceb4bb285defc0b38b611c06f8837ee5fc7cc
albertomonterom <72234574+albertomonterom@users.noreply.github.com>
2021-05-04 17:56:31 -0500

    Update velocidad_principal.py

--- velocidad_principal.py
+++ velocidad_principal.py
@@ -5,82 +5,152 @@ Created on Tue May  4 11:52:37 2021
 @author: molin
 """
 
-import matplotlib.pyplot as plt
-import pandas as pd
-import csv
-import statistics as stats
+
 import numpy as np
 import matplotlib.dates as mdates
 import datetime as dt
 import pylab as pl
+import matplotlib.pyplot as plt
+import pandas as pd
+import csv
+import statistics as stats
+
 
 
 global datos
+
+
 datos=pd.read_csv('datos.csv')
+
+
+
 global df
+
+
+
+
+
 df=pd.DataFrame(datos)
+
 
 
 import statistics as stats
 def vbarra():
 
     tab = pd.crosstab(index=df['velocidad'],
+                      
                       columns='Frecuencia')
+    
     velocidad = [ i for i in df['velocidad']]
 
+    
+    
+    
+    
     data = velocidad
+    
     result = []
+    
     for i in data:
+        
                 if i not in result:
+                
                                 result.append(i)
+                    
+                    
+                    
+                    
 
     dataLimpio=[]
     for i in result:
                 
                 if i > 0:
+                    
                                 dataLimpio.append(i)
 
     dataLimpio.sort()
+    
 
     
     freq = [ item for item in tab['Frecuencia']]
+    
+    
     x = dataLimpio
+    
+    
     energy = freq
+    
 
     x_pos = [i for i, _ in enumerate(dataLimpio)]
 
     plt.bar(x_pos, freq, color='#660708')
+    
     plt.xticks(x_pos, dataLimpio)
+    
     plt.show()
 
+    
+    
+    
+    
 
 def vPastel():
     tab = pd.crosstab(index=df['velocidad'],
                       
+                      
                       columns='Frecuencia')
+    
+    
     velocidad = [ i for i in df['velocidad']]
+    
     data = velocidad
+    
     result = []
+    
     for i in data:
+        
                 if i not in result:
+                
+                
+                
+                
                                 result.append(i)
 
     dataLimpio=[]
+    
     for i in result:
+        
                 
+            
                 if i > 0:
+                    
                                 dataLimpio.append(i)
+                        
+                        
+                        
     dataLimpio.sort()
+    
     freq = [ i for i in tab['Frecuencia']]
+    
     labels = dataLimpio
+    
     sizes = freq
+    
     fig1, ax1 = plt.subplots()
+    
     ax1.pie(sizes, labels=labels,
+            
+            
             autopct='%1.1f%%',
+            
             shadow=True, 
+            
             startangle=90)
+    
     ax1.axis('equal')
+    
     plt.show()
+    
 
 def vFAcumulada():
     tab = pd.crosstab(index=df['velocidad'],
